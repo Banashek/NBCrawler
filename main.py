6921cb1c8228407548a146d38943f5cbdfd59c4f
Jonathan Banashek <jbanashek@gmail.com>
2015-05-19 17:48:12 -0700

    Adding in logic to check and grab results from the database before making a url request

--- main.py
+++ main.py
@@ -26,56 +26,39 @@ except Exception as e:
 cursor = db.googleResults.find({"searchQuery": search_query})
 
 if cursor.count() > 1:
-    pass
+    print("found results in database")
 else:
-    pass
+    # Build the url
+    url_base = "http://www.google.ca/search?" 
+    hl_param = "hl=en" 
+    query_param = "&q=" + search_query
+    btng_param = "&btnG=Google+Search"
+    meta_param = "&meta="
+    num_results_param = "&num=" + num_results.__repr__()
+    url = url_base + hl_param + query_param + btng_param + meta_param + num_results_param
 
+    headers = { 'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36' }
 
-# Build the url
-url_base = "http://www.google.ca/search?" 
-hl_param = "hl=en" 
-query_param = "&q=" + search_query
-btng_param = "&btnG=Google+Search"
-meta_param = "&meta="
-num_results_param = "&num=" + num_results.__repr__()
-url = url_base + hl_param + query_param + btng_param + meta_param + num_results_param
+    # Create request object with url and custom headers
+    r = requests.get(url, headers=headers)
 
-headers = { 'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36' }
+    soup = BeautifulSoup(r.text)
+    results_array = []
 
-# Create request object with url and custom headers
-r = requests.get(url, headers=headers)
-
-soup = BeautifulSoup(r.text)
-results_array = []
-
-# Create GoogleResult objects from resulting page
-for result in soup.find_all('li', class_="g"):
-    link = result.find('a')
-    link_title = link.get_text()
-    # Filter out image and ad results that have a blank title
-    if not link_title:
-        continue
-    link_href = link.get('href')
-    # Check if subtext is there
-    if result.find('span', class_='st') is not None:
-        link_subtext = result.find("span", class_="st").get_text()
-    else:
-        link_subtext = "No subtext for this node"
-    search_terms = search_query.split(' ')
-    gres = GoogleResult(search_query, link_title, link_href, link_subtext, ["test", "test2", "test3"])
-    gres.save(db)
-    results_array.append(gres)
-
-
-# for gr in results_array:
-#     print("Search term: " + gr.searchterm)
-#     print("Title: " + gr.title)
-#     print("Link: " + gr.link)
-#     print("Subtext: " + gr.subtext)
-#     print()
-
-
-
-
-# url = "http://www.google.ca/search?hl=en&q=nbc+universal&btnG=Google+Search&meta="
-# url = "https://duckduckgo.com/?q=nbc"
+    # Create GoogleResult objects from resulting page
+    for result in soup.find_all('li', class_="g"):
+        link = result.find('a')
+        link_title = link.get_text()
+        # Filter out image and ad results that have a blank title
+        if not link_title:
+            continue
+        link_href = link.get('href')
+        # Check if subtext is there
+        if result.find('span', class_='st') is not None:
+            link_subtext = result.find("span", class_="st").get_text()
+        else:
+            link_subtext = "No subtext for this node"
+        search_terms = search_query.split(' ')
+        gres = GoogleResult(search_query, link_title, link_href, link_subtext, ["test", "test2", "test3"])
+        gres.save(db)
+        results_array.append(gres)
